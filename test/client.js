3e85167e7f882003943ac4ad04a5e99558906e86
Tim Scarborough <scarborough.tim@gmail.com>
2021-09-22 20:26:48 -0400

    Update client.js

--- test/client.js
+++ test/client.js
@@ -3,8 +3,9 @@ const { Client } = require('pg');
 const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres'
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
 });
 
 console.log('process.env.POSTGRES_HOST = ' + process.env.POSTGRES_HOST);
